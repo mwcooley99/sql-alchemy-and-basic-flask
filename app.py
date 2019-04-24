204fafd669bacac31f5f931e4111097f8fe75079
Matt <mwcooley99@gmail.com>
2019-04-24 15:58:21 -0700

    Now using flask-sqlalchemy for the database

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 import os
-
 from flask import Flask, jsonify, render_template
-
-from sqlalchemy import create_engine, func, inspect
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import func
+from sqlalchemy.ext.automap import automap_base
+
 
 import datetime as dt
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Set up Models
-engine = create_engine('sqlite:///hawaii.sqlite')
-Session = sessionmaker(bind=engine)
-session = scoped_session(Session)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'hawaii.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Add extensions to the app
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
-# Create the Base and models
+# Create the Base
 Base = automap_base()
-Base.prepare(engine, reflect=True)
+Base.prepare(db.engine, reflect=True)
 
+# Create the Models
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
@@ -37,14 +38,14 @@ def index():
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    precipitation_data = session.query(Measurement.date,
+    precipitation_data = db.session.query(Measurement.date,
                                        Measurement.prcp).all()
     return jsonify([{k: v} for k, v in precipitation_data])
 
 
 @app.route('/api/v1.0/stations')
 def stations():
-    stations = [s.__dict__ for s in session.query(Station).all()]
+    stations = [s.__dict__ for s in db.session.query(Station).all()]
     for station in stations: station.pop('_sa_instance_state', None)
 
     return jsonify(stations)
@@ -53,7 +54,7 @@ def stations():
 @app.route('/api/v1.0/tobs')
 def tobs():
     # Find the last data point
-    most_recent_date, = session.query(Measurement.date).order_by(
+    most_recent_date, = db.session.query(Measurement.date).order_by(
         Measurement.date.desc()).first()
 
     # Get the start date
@@ -62,7 +63,7 @@ def tobs():
     start_date = start_date.strftime('%Y-%m-%d')
 
     # Query the data
-    precipitation_data = session.query(Measurement.date, Measurement.tobs) \
+    precipitation_data = db.session.query(Measurement.date, Measurement.tobs) \
         .filter(Measurement.date >= start_date,
                 Measurement.date <= most_recent_date) \
         .order_by(Measurement.date).all()
@@ -71,7 +72,7 @@ def tobs():
 
 @app.route('/api/v1.0/<start>')
 def stats_from_start(start):
-    query = session.query(func.min(Measurement.tobs),
+    query = db.session.query(func.min(Measurement.tobs),
                           func.avg(Measurement.tobs),
                           func.max(Measurement.tobs)).filter(
         Measurement.date >= start).first()
@@ -82,7 +83,7 @@ def stats_from_start(start):
 
 @app.route('/api/v1.0/<start>/<end>')
 def stats_from_start_end(start, end):
-    query = session.query(func.min(Measurement.tobs),
+    query = db.session.query(func.min(Measurement.tobs),
                           func.avg(Measurement.tobs),
                           func.max(Measurement.tobs)).filter(
         Measurement.date >= start, Measurement.date <= end).first()
